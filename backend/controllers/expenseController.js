54bd2acbbebcb81c5cd758bde7d9db7668ab780b
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-05-05 09:03:45 +0530

    Working on Expense Route

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -19,3 +19,56 @@ export const addExpense = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+export const getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find({ user: req.user.id })
+      .sort({ createdAt: -1 });
+
+    res.json(expenses);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const updateExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+
+    if (!expense) return res.status(404).json({ msg: "Expense not found" });
+
+    // check ownership
+    if (expense.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    const updated = await Expense.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updated);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Delete expense
+export const deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+
+    if (!expense) return res.status(404).json({ msg: "Expense not found" });
+
+    if (expense.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await expense.deleteOne();
+
+    res.json({ msg: "Expense deleted" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { addExpense } from "../controllers/expenseController.js";
+import {
+  addExpense,
+  getExpenses,
+  updateExpense,
+  deleteExpense,
+} from "../controllers/expenseController.js";
+
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// CREATE
 router.post("/", authMiddleware, addExpense);
+
+// READ
+router.get("/", authMiddleware, getExpenses);
+
+// UPDATE
+router.put("/:id", authMiddleware, updateExpense);
+
+// DELETE
+router.delete("/:id", authMiddleware, deleteExpense);
 
 export default router;
